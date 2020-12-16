ae1dd8d5fd074cad4114d5ad790c8bd2cdc8e450
dominicdaviescodes <codewithdom@gmail.com>
2020-12-16 21:50:38 +0000

    bring in DOM elements to js file

---
+++ app.js
@@ -0,0 +1,10 @@
+// bring in what we want from the DOM
+
+// small cups go blue or back to white and affect liters and % numbers
+const smallCups = document.querySelectorAll('.cup-small');
+
+const liters = document.getElementById('liters');
+// when we click a small cup/s the liter value (%) in large cup changes up or down
+const percentage = document.getElementById('percentage');
+// the water will move up or down
+const remained = document.getElementById('remained');
