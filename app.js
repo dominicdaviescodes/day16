4e6e9a08f7efb24394a9f03025edd774ad3eb441
dominicdaviescodes <codewithdom@gmail.com>
2020-12-16 22:04:32 +0000

    update small cups

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 // the water will move up or down
 const remained = document.getElementById('remained');
+
+// Fill the small cups
+
+smallCups.forEach((cup, idx) => {
+  // console.log(idx); gives us index of cups
+  cup.addEventListener('click', () => highlightCups(idx));
+});
+
+// add full class to small cups when clicked
+function highlightCups(idx) {
+  // if you click on a full cup, it will empty, previous cups are still full
+  // toggle cups. if you click a full cup it will empty and other cups will empty
+  if (
+    smallCups[idx].classList.contains('full') &&
+    !smallCups[idx].nextElementSibling.classList.contains('full')
+  ) {
+    idx--;
+  }
+  // use idx2 as specific to this loop
+  smallCups.forEach((cup, idx2) => {
+    // fill cup and all ones before it
+    // is this index <= the index we click on?
+    if (idx2 <= idx) {
+      cup.classList.add('full');
+    } else {
+      cup.classList.remove('full');
+    }
+  });
+}
